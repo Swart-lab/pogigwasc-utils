8413b83b34ccaca015b544818dd6b6c093119bf7
Kwee Boon Brandon Seah <kb.seah@tuebingen.mpg.de>
2021-01-13 18:06:39 +0100

    Rewrite split scaffolds script
     * Use Biopython to manage sequence object
     * Output as JSON for easier import for recombine step

--- split_scaffolds.py
+++ split_scaffolds.py
@@ -2,89 +2,98 @@
 
 import re
 import argparse
+import json
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--input", type=str, 
     help="Fasta file containing contigs and scaffolds")
 parser.add_argument("-o", "--output", type=str, default="test_scaffolds_split",
     help="Output filename prefix")
-parser.add_argument("-w", "--width", type=int, default=80,
-    help="Fasta file output width")
 args = parser.parse_args()
 
-def wrap_line_buffer(line, fh, width=80):
-    """Wrap a line to desired width, write full width lines, and return remainder
-    that does not fill complete line"""
-    if len(line) > width:
-        for rep in range(int(len(line)/width)): # floor line length / width
-            fh.write(line[rep*width : (rep+1)*width] + "\n")
-        return(line[int(len(line)/width)*width:])
-    else:
-        return(line)
 
-tbl = []
-# list of lists, each with three fields
-# new contig id / old contig id / coordinate offset of new vs old contig
+def get_negative_coordinates(pos : list, start : int, end : int):
+    """
+    get coordinates for the complement to a set of regions
 
-fh_in = open(args.input, "r")
-fh_out = open(args.output + ".fasta", "w")
+    Regions are defined by a list of pairs of tuples. In addition the start and
+    end coordinate for the encompassing region must be provided. Assume that
+    coordinates are 0-based end-exclusive (python convention).
+    """
+    pos = sorted(pos, key = lambda x : x[0]) # sort by start coordinate
+    segs = [0]
+    for (segstart, segend) in pos:
+        segs.extend([segstart, segend])
+    segs.append(end)
+    return(list(zip(segs[::2], segs[1::2])))
 
-ctg = None
-newctg = None
-linebuff = ""
-coord = 0
-for line in fh_in:
-    line = line.rstrip()
-    if re.match(">", line):
-        # Dump data from old contig
-        if ctg:
-            fh_out.write(linebuff + "\n")
-            linebuff = "" # Clear buffer
-            coord = 0
-        # Start new contig
-        ctg = line[1:] # strip starting > character
-        splitcount = 0
-        fh_out.write(">" + ctg + "\n")
-    else:
-        ns = re.finditer(r"N+", line)
-        ncoords = [i.span() for i in ns]
-        if len(ncoords) > 0:
-            # Remove Ns and record coordinates
-            # Add to line buffer
-            start = 0
-            for (nstart, nend) in ncoords:
-                linebuff += line[start:nstart]
-                linebuff = wrap_line_buffer(linebuff, fh_out, args.width)
-                if len(linebuff) > 0:
-                    fh_out.write(linebuff + "\n")
-                linebuff = ""
-                if nend < len(line):
-                    # Start a new record
-                    splitcount += 1
-                    newctg = ctg + "_" + str(splitcount)
-                    fh_out.write(">" + newctg + "\n")
-                    newcoord = coord + nend
-                    tbl.append([newctg, ctg, newcoord])
-                # Update start coordinate for next non-N segment
-                start = nend
-            if start < len(line):
-                # catch trailing sequence that is not N
-                linebuff += line[start:]
-                linebuff = wrap_line_buffer(linebuff, fh_out, args.width)
-            coord += len(line)
+
+def scaffold2contig_coords(seq_dict):
+    """Report non-N regions in input set of scaffolds
+
+    If a sequence has no Ns, the the name is passed through unchanged. Otherwise
+    a number is appended to the original scaffold name, to make name of each
+    segment derived from splitting up a scaffold on Ns.
+    
+    Parameters
+    ----------
+    seq_dict : dict
+        Dict of Bio.SeqRecord objects representing scaffolds of interest, keyed by
+        scaffold name
+
+    Returns
+    -------
+    dict
+        keyed by new segment name. value is a dict with keys `orig, start, end`
+        describing segment position on origial scaffold
+    """
+    new2old = {}
+    for ctg in seq_dict:
+        # find stretches of Ns
+        nn = re.finditer(r"N+", str(seq_dict[ctg].seq))
+        nn_spans = [hit.span() for hit in nn]
+        if nn_spans:
+            pos_spans = get_negative_coordinates(nn_spans, 0, len(seq_dict[ctg]))
+            for i in range(len(pos_spans)):
+                newctg = f"{ctg}_{str(i)}"
+                new2old[newctg] = { 'orig' : ctg, 
+                                    'start' : pos_spans[i][0],
+                                    'end' : pos_spans[i][1] }
         else:
-            # Add to existing line
-            linebuff += line
-            coord += len(line)
-            linebuff = wrap_line_buffer(linebuff, fh_out, args.width)
-# Print trailing buffer sequence
-if len(linebuff) > 0:
-    fh_out.write(linebuff + "\n")
-# Close file handles
-fh_in.close()
-fh_out.close()
+            # pass through as is
+            new2old[ctg] = { 'orig' : ctg, 'start' : 0, 'end' : len(seq_dict[ctg]) } 
+    return(new2old)
 
-# Write table of coordinate offsets for new contigs
-with open(args.output + "table.tsv", "w") as fh:
-    for l in tbl:
-        fh.write("\t".join([str(i) for i in l]) + "\n")
+
+def report_split_seqs(seq_dict, new2old):
+    """Report sequences of contigs split from scaffolds
+
+    Parameters
+    ----------
+    seq_dict : dict
+    new2old : dict
+        Output from scaffold2contig_coords()
+    """
+    seqlist = []
+    for ctg_id in new2old:
+        orig = new2old[ctg_id]['orig']
+        start = new2old[ctg_id]['start']
+        end = new2old[ctg_id]['end']
+        ctg = SeqRecord(
+            seq_dict[orig].seq[start:end],
+            id = ctg_id,
+            name = ctg_id,
+            description = "")
+        seqlist.append(ctg)
+    return(seqlist)
+
+
+if __name__ == "__main__": 
+    asm = SeqIO.to_dict(SeqIO.parse(args.input, 'fasta'))
+    spls = scaffold2contig_coords(asm)
+    asm_new = report_split_seqs(asm, spls)
+    SeqIO.write(asm_new, f"{args.output}.fasta", "fasta")
+    with open(f"{args.output}.json", "w") as fh:
+        fh.write(json.dumps(spls, indent=2))
